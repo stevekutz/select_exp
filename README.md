9be2ece22e36956d0e4d6d42f2219be3e3eb5578
stevekutz <stkutz@gmail.com>
2019-11-15 23:07:43 -0600

    built handlers

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 1) ### Set up React app `yarn create react-app app`
 
 2) ### Add Dependencies  
-    `yarn add moment react-dom react-loader-spinner react-rainbow-components react-router-dom react-scripts react-spinners-kit reactn reactn-devtools semantic-ui-react styled-components redux reinstate redux-devtools-extension react-select`  
+    `yarn add moment react-dom react-loader-spinner react-rainbow-components react-router-dom react-scripts react-spinners-kit reactn reactn-devtools semantic-ui-react styled-components redux reinspect redux-devtools-extension react-select`  
     
     - For `react-loaded-spinner`, you must also add `styled-components`
     - For `semantic-ui-react`, you must also 
--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+// import logo from './logo.svg';
+import {useState} from 'reinspect';
 import './App.css';
 
-function App() {
+const App = () => {
+  const initial = {selectedKey: null};
+
+  const [dropDownVal, setDropDownVal] = useState(initial, "DropDownValues")
+
+  const updateDropDown = value => {
+    setDropDownVal({... formVal, selectedKey: value});  
+  }
+
+  resetDropDown = () => {
+    setDropDownVal(initial)
+  }
+
+  const options = [
+    { value: 1, label: "Low" },
+    { value: 2, label: "Medium" },
+    { value: 3, label: "High"}
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+
+
     </div>
   );
 }
